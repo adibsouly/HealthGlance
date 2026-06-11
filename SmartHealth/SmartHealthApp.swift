3e08c4c11e7753fa06a504e9d695d0efadfa9630
adib.souly@gmail.com <Danesh@1364>
2026-06-10 22:51:36 -0400

    Initial Commit

---
+++ SmartHealth/SmartHealthApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartHealthApp.swift
+//  SmartHealth
+//
+//  Created by Adib Souly on 2026-06-10.
+//
+
+import SwiftUI
+
+@main
+struct SmartHealthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
